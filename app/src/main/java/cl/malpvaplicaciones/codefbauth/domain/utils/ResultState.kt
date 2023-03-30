49270f4342a1299e67d154502e46021609634349
Marcos Alexander <malpvdeveloper@gmail.com>
2023-03-30 20:01:29 -0300

    feat: Se crea la clase ResultState para indicar los resultados de las operaciones

---
+++ app/src/main/java/cl/malpvaplicaciones/codefbauth/domain/utils/ResultState.kt
@@ -0,0 +1,6 @@
+package cl.malpvaplicaciones.codefbauth.domain.utils
+
+sealed class ResultState<out T> {
+    data class Success<out T>(val data: T) : ResultState<T>()
+    data class Error(val exception: Exception) : ResultState<Nothing>()
+}
