ca7478150ef1a2794208f51ed643ecccd2cf15d1
Marcos Alexander <malpvdeveloper@gmail.com>
2023-03-30 20:04:03 -0300

    feat: Se crean los casos de uso

---
+++ app/src/main/java/cl/malpvaplicaciones/codefbauth/domain/usecases/LoginUseCase.kt
@@ -0,0 +1,21 @@
+package cl.malpvaplicaciones.codefbauth.domain.usecases
+
+import cl.malpvaplicaciones.codefbauth.data.repositories.AuthenticationRepository
+import cl.malpvaplicaciones.codefbauth.domain.models.User
+import cl.malpvaplicaciones.codefbauth.domain.utils.ResultState
+
+class LoginUseCase (
+    private val authenticationRepository: AuthenticationRepository
+) {
+
+    suspend operator fun invoke(
+        email: String,
+        password: String
+    ): ResultState<User> {
+        return when (val result = authenticationRepository.login(email, password)) {
+            is ResultState.Success -> ResultState.Success(result.data)
+            is ResultState.Error -> ResultState.Error(result.exception)
+        }
+    }
+
+}
